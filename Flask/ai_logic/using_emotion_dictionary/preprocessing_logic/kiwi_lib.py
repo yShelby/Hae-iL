56c0a7fc62037ca78e80989e9e3d37ecbc31ff9d
yShelby <124975121+yShelby@users.noreply.github.com>
2025-07-07 09:29:55 +0900

    Delete Flask/ai_logic directory
    로직 변경으로 인한 파일 전체 삭제

--- Flask/ai_logic/using_emotion_dictionary/preprocessing_logic/kiwi_lib.py
+++
@@ -1,4 +0,0 @@
-from kiwipiepy import Kiwi #(필요시)pip install kiwipiepy
-
-# kiwi library 초기화 (2군데에서 쓰므로 초기화 1번)
-kiwi = Kiwi()
--- Flask/ai_logic/using_emotion_dictionary/preprocessing_logic/whitespace_replacer.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def replace_whitespace(text: str) -> str:
-    text = re.sub(r"\s+", " ", text)
-    return text.strip() 
