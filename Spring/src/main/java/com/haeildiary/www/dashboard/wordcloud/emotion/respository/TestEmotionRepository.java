1a502b8df6cb289a5dbd8f862f5d740f324eecbb
daran <moon.daran.s@gmail.com>
2025-07-28 13:47:27 +0900

    Dash_J2 wordcloud 부분 병합 - DR

--- Spring/src/main/java/com/haeildiary/www/dashboard/wordcloud/emotion/respository/TestEmotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.haeildiary.www.dashboard.wordcloud.emotion.respository;
-
-import com.haeildiary.www.dashboard.wordcloud.emotion.entity.TestMoodDetailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestEmotionRepository extends JpaRepository<TestMoodDetailEntity, Long> {
-}
