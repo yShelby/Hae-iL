a372f7937411d9cb8506f1a4ca74c405ad0d3b7a
kmc-726 <anscjf23@naver.com>
2025-08-11 14:19:38 +0900

    영화 추천 서비스 성인영화 필터링 추가

--- Spring/src/main/java/com/haeildiary/www/recommend/movie/movieservice/TmdbApiClientService.java
+++ Spring/src/main/java/com/haeildiary/www/recommend/movie/movieservice/TmdbApiClientService.java
@@ -34,7 +34,7 @@ public class TmdbApiClientService {
     }
 
     public List<MovieDto> searchMoviesByGenre(Integer genreCode) {
-        List<String> bannedKeywords = List.of("softcore", "pink film", "adult", "sex");
+        List<String> bannedKeywords = List.of("softcore", "pink film", "adult", "sex", "lgbt", "lesbian", "gay");
 
         // 비동기 스트림을 block()으로 동기 변환해서 리턴
         return webClient.get()
